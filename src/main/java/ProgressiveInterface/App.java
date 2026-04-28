107bffe507b4f30a00f66a1943f6be0fc5fd282c
adriElias <34091624+adriElias@users.noreply.github.com>
2026-04-28 12:58:53 +0200

    build: update dependencies in pom.xml

---
+++ src/main/java/ProgressiveInterface/App.java
@@ -0,0 +1,13 @@
+package ProgressiveInterface;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
